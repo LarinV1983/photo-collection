7cf3f6ca60c21f92f1e9e5124b5301631ebe1fc8
Viktor Larin <Larinv1983@mail.ru>
2022-11-08 11:33:16 +0300

    сделал категории

--- src/App.js
+++ src/App.js
@@ -2,32 +2,43 @@ import React from 'react';
 import Collection from './components/collection';
 import './index.scss';
 
+const cats = [
+    { "name": "Все" },
+    { "name": "Море" },
+    { "name": "Горы" },
+    { "name": "Архитектура" },
+    { "name": "Города" }
+  ]
+
 function App() {
 const [searchValue, setSearchValue] = React.useState('');
 const [collections, setCollections] = React.useState([]);
+const [category, setCategory] = React.useState(0);
 
-React.useEffect(() => {
-  fetch('')
-  .then((res)=> res.json())
-  .then((json) => {
-    setCollections(json);
-  })
-  .catch((err) => {
-    console.warn(err);
-    alert('Ошибка при получении данных');
-  });
-}, []);
+// React.useEffect(() => {
+//   fetch('')
+//   .then((res)=> res.json())
+//   .then((json) => {
+//     setCollections(json);
+//   })
+//   .catch((err) => {
+//     console.warn(err);
+//     alert('Ошибка при получении данных');
+//   });
+// }, []);
 
   return (
     <div className="App">
     <h1>Моя коллекция фотографий</h1>
     <div className='top'>
     <ul className='tags'>
-      <li className='active'>Все</li>
-      <li>Горы</li>
-      <li>Море</li>
-      <li>Архитектура</li>
-      <li>Города</li>
+   {cats.map((obj, index) => (
+    <li onClick={()=> setCategory(index)}
+    className={category=== index ? 'active' : ''} 
+    key={obj.name}>
+    {obj.name}
+    </li>
+    ))}
     </ul>
      <input value ={searchValue} onChange={(e) => setSearchValue(e.target.value)} 
      className='search-input' type="text" placeholder='Поиск по названию'/> 
